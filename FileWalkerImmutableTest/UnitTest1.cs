fe072d8afc3a1e8582edf8e2ba999762985eb37e
Janik Lesage <mrjaniklesage@gmail.com>
2024-11-26 11:48:39 -0500

    Fixed the project from the teacher, now we can do tests

---
+++ FileWalkerImmutableTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Immutable;
+using NUnit.Framework;
+
+
+namespace FileWalkerImmutableTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
